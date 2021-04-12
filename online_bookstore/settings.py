30f92973c211b762188d2222761b4ebb1bafb3b4
Utsabab <utsab.khakurel@bison.howard.edu>
2021-04-11 19:49:06 -0700

    base.html home page works

--- online_bookstore/settings.py
+++ online_bookstore/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
 
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
